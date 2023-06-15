386d0fa901233dc94ccfd61deeb6519a6fe65d7c
tishanarola <133970270+tishanarola@users.noreply.github.com>
2023-06-15 18:47:02 +0530

    Add files via upload

---
+++ PRQ6.C
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+int i;
+clrscr();
+for(i=1;i<=7;i++){
+if(i==1 || i==7){
+printf("* * * *\n");
+}else if(i==2){
+printf("     *\n");
+}else if(i==3){
+printf("    *\n");
+}else if(i==4){
+printf("   *\n");
+}else if(i==5){
+printf("  *\n");
+}else{
+printf(" *\n");
+}
+}
+getch();
+}
+
+/*
+    *****
+ **       **
+**         **
+**         **
+ **    **  **
+    *****
+	  **
+	   **
+*/
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+int i,j;
+clrscr();
+for(i=1;i<=7;i++){
+if(i==1 || i== || i==7){
+printf("* * * * *\n");
+}else{
+printf("*      *\n");
+}
+}
+getch();
+}
